fd1a7409b93dd0fe10874f2332f153321e4e5e5f
sigitsedayu <sigitsedayu@gmail.com>
2018-07-27 21:57:59 +0700

    second commit

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ router.get('/hello-world', function (req, res) {
 .response(['text/plain'], 'A generic greeting.')
 .summary('Generic greeting')
 .description('Prints a generic greeting.');
+
+const joi = require('joi');
+
+router.get('/hello/:name', function (req, res) {
+  res.send(`Hello ${req.pathParams.name}`);
+})
+.pathParam('name', joi.string().required(), 'Name to greet.')
+.response(['text/plain'], 'A personalized greeting.')
+.summary('Personalized greeting')
+.description('Prints a personalized greeting.');
