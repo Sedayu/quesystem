f95916b48320fb36c0125efcb6226678844d12ac
sigitsedayu <sigitsedayu@gmail.com>
2018-08-14 12:16:11 +0700

    udpate api

--- index.js
+++ index.js
@@ -59,15 +59,16 @@ router.get('/checkup_status/:doctor_id/:date/:status', function(req,res){
   // `);
   // res.send(keys);
 
-  if (req.pathParams.status==="all") {
+  if (req.pathParams.status =="all") {
      const keys = db._query(aql`
       FOR entry IN checkup
       FOR u IN users
       FOR d IN doctors
       SORT entry.que_number ASC
-      FILTER u._key == entry.user_id && d._key == entry.doctor_id && entry.doctor_id == ${req.pathParams.doctor_id} && entry.date == ${req.pathParams.date} }
+      FILTER u._key == entry.user_id && d._key == entry.doctor_id && entry.doctor_id == ${req.pathParams.doctor_id} && entry.date == ${req.pathParams.date} 
       RETURN { checkup: entry, users: u , doctors: d}
     `);
+     res.send(keys);
   } else {
      const keys = db._query(aql`
       FOR entry IN checkup
@@ -77,10 +78,11 @@ router.get('/checkup_status/:doctor_id/:date/:status', function(req,res){
       FILTER u._key == entry.user_id && d._key == entry.doctor_id && entry.doctor_id == ${req.pathParams.doctor_id} && entry.date == ${req.pathParams.date} && entry.status == ${req.pathParams.status}
       RETURN { checkup: entry, users: u , doctors: d}
     `);
+     res.send(keys);
   }
 
  
-  res.send(keys);
+  
 })
 .response(joi.array().items(joi.string().required()).required(), 'List of keys')
 .summary('Retrieve an entry')
